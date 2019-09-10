d9235d9e3665877ee0a5a05dab6f40dd8a31734b
Denis Beketov <beketovdo97@yandex.ru>
2019-09-10 17:56:01 +0300

    Update random.cpp

--- random.cpp
+++ random.cpp
@@ -41,6 +41,20 @@ int * fortest(int * souse) {
     return mh;
 }
 
+int fortest(int one, int two, int tree, int four) {
+    int * mh = new int[5];
+    //int * souse = new int[4];
+    int souse[] = {one, two, tree, four};
+    for (unsigned int i = 0; i < 1000000; i++) {
+        mh = mhack(mh, i);
+        if (test(souse, mh)) {
+            return mh[4];
+        }
+    }
+    log << "ERROR" << endl;
+    return -1;
+}
+
 bool check(int * souse) {
     int * number = fortest(souse);
     if (number[4] == souse[4]) {
